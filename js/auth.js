3ad3b0ae749445c8aa98fa270489ebbfed272b29
Gunnar Lehker <lehker@mindsquare.de>
2017-05-02 20:54:58 +0200

    Update for Salesforce Mobile SDK 5.1.0

--- js/auth.js
+++ js/auth.js
@@ -1,54 +1,41 @@
+// Creating the application namespace
+var app = {
+    models: {},
+    views: {},
+    utils: {}
+};
+
 jQuery(document).ready(function() {
-    // Container
-    if (window.cordova && !cordova.interceptExec) {
-        document.addEventListener("deviceready", function() {
-            console.log("onDeviceReady: cordova ready");
+    //Add event listeners and so forth here
+    console.log("onLoad: jquery ready");
+    // FastClick
+    new FastClick(document.body);
 
-            //Call getAuthCredentials to get the initial session credentials
-            cordova.require("com.salesforce.plugin.oauth").getAuthCredentials(
-                function(creds) {
-                    appStart( _.extend(creds, {userAgent: navigator.userAgent}), cordova.require("com.salesforce.plugin.oauth").forcetkRefresh);
-                }, 
-                function(error) { 
-                    console.log("Auth failed: " + error); 
-                });
-        });
-    }
     // Browser
-    else {
-        var loginUrl = "https://test.salesforce.com/";
-        var consumerKey = "yourConsumerKey";
-        var callbackUrl = "http://localhost:9000/";
-
-        // Instantiating forcetk ClientUI
-        var oauthClient = new ForceOAuth(loginUrl, consumerKey, callbackUrl,
-             function forceOAuthUI_successHandler(forcetkClient) { // successCallback
-                 console.log('OAuth success!');
-                        console.log(oauthClient.oauthResponse);
-                 creds = {
-                    clientId: consumerKey,
-                    loginUrl: loginUrl,
-                    proxyUrl: callbackUrl,
-                    accessToken: oauthClient.oauthResponse.access_token,
-                    instanceUrl: oauthClient.oauthResponse.instance_url
-                };
-                 creds = _.extend(creds, oauthClient.oauthResponse);
-                 appStart(creds);
-             },
-             function forceOAuthUI_errorHandler(error) { // errorCallback
-                 console.log('OAuth error!');
-                 if (confirm("Authentication Failed. Try again?")) oauthClient.login();
-             });
-
-        oauthClient.login();
+    if (cordova.interceptExec) {
+        force.init({loginURL: "https://test.salesforce.com/",
+                    appId: "3MVG9sLbBxQYwWqvigZkG451mOeRQ1HZ0.CMHfbrXp0v5m1gOwd9BjeA0tQRidGJn5MndsavpBExwVfmS4D7r",
+                    oauthCallbackURL: "http://localhost:9000/oauthcallback.html",
+                    useCordova: false 
+                   });
     }
+       
+    force.login(
+        function(event) {
+            console.log("Auth succeeded"); 
+            appStart();
+        },
+        function(error) {
+            console.log("Auth failed: " + error); 
+        }
+    );
 });
 
 function appStart(creds, refresh)
 {
     // Force init
-    Force.init(creds, null, null, refresh);
+    Force.init();
 
-    var event = new Event('loggedIn');
-    document.dispatchEvent(event);
+    // Go!
+    Backbone.history.start();
 }
